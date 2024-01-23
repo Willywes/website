8203a1049a9bb9867cf809431e5e8b5e2668948a
Willywes <willywes@hotmail.cl>
2024-01-22 22:32:37 -0300

    Update Home page and remove WhatIHaveLearned section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				'handWave': {
+					'0%': { transform: 'rotate(-10deg)' },
+					'25%': { transform: 'scale(0.9)  rotate(10deg)' },
+					'50%': { transform: 'rotate(-15deg)' },
+					'75%': { transform: ' rotate(10deg)' },
+					'100%': { transform: 'rotate(-10deg)' },
+				},
+			},
+			animation: {
+				'handWave': 'handWave 2s ease-in-out infinite',
+			},
+		},
 	},
 	plugins: [],
 }
